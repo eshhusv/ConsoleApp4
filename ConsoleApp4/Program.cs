1a1b8028c593482ee3da577072056e3e162cca6c
1 <1@DESKTOP-102BEE2>
2022-10-12 16:17:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Коваленок, 22-ИСП-2/2, 5.83
+Console.WriteLine("Введите вещественное число: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень числа");
+int n = int.Parse(Console.ReadLine());
+double p = 1;
+for (int i = 1; i <= n; i++)
+{
+    p *= a;
+}
+Console.WriteLine($"{p:F2}"); 
